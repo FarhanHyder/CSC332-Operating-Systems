de36bca881bfc4b45965062dd531abe8f7ec9411
FarhanHyder <fmshyder@gmail.com>
2018-10-09 20:15:01 -0400

    task3_part1: final

--- LAB/Task3/task3_part1.c
+++ LAB/Task3/task3_part1.c
@@ -1,19 +1,24 @@
- #include <unistd.h>    //execl
+/*  task3 : part #1
+    author: Farhan Hyder
+    submission date: 10/11/2018
+    description: 
+		- creates a child that executes a command which tells time and date in Unix
+*/
+
+#include <unistd.h>    //execl
+#include <stdio.h>     //printf
 
 
-int main()
+int main(int argc, char *argv[])
 {
-    pid_t child = fork();
+    printf("Parent PID #[%d]\n",getpid());
 
+    pid_t child = fork();    
     if (child==0)
     //child stuffs
     {
-        
-    }
-    else
-    //parent stuffs
-    {
-
+        printf("Child PID #[%d]\n",getpid());
+        execl("/bin/date", argv[0],NULL);
     }
     
 }
