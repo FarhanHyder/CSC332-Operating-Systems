0dca187f909fc7c86a0ddac222f2204fe4c8541a
FarhanHyder <fmshyder@gmail.com>
2018-10-11 08:18:27 -0400

    task3 : parent modified

--- LAB/Task3/Parent_Prcs.c
+++ LAB/Task3/Parent_Prcs.c
@@ -2,9 +2,32 @@
     author: Farhan Hyder
 */
 
+#include<stdio.h>		//printf
+#include <unistd.h>		//fork
+#include <sys/types.h>	//fork, waitpid
+#include <sys/wait.h>	//waitpid
+
 int main(int argc, char const *argv[])
 {
-    
+    pid_t child1, child2;
+    int *child1_status, *child2_status;
+
+    child1 = fork();
+    if (child1 == 0)
+    //child 1 process
+    {
+        //TODO: execute Prcs_P1 
+        printf("\nchild 1\n");
+
+        child2 = fork();
+        if (child2 == 0)
+        {
+            //TODO: execute Prcs_P2
+            printf("\nchild 2\n");
+        }
+
+    }
+
     return 0;
 }
 
